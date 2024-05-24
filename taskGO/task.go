42f0eb9376a8ae61b06f6796111c96278c1dc3be
abuzar <mabuzar533@gmail.com>
2024-05-24 17:07:58 +0500

    update task.go

--- taskGO/task.go
+++ taskGO/task.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Jsons() {
+
 	p1 := Employees{
+
 		e_id:        1,
 		name:        "John Doe",
 		departments: "Engineering",
@@ -38,52 +42,67 @@ func Jsons() {
 		},
 	}
 
-	p2 := Employees{
-		e_id:        2,
-		name:        "Jane Smith",
-		departments: "Marketing",
-		skills:      []string{"Digital Marketing", "Content Creation", "SEO"},
-		projects: []Projects{{
-			name_p:   "Campaign A",
-			duration: 4,
-			team: []Team{{
-				t_id: 5,
-				role: "Marketing Manager"}, {
-				t_id: 6,
-				role: "Content Writer",
-			},
-			},
-		},
-		},
+	fmt.Println("Employee ID: ", p1.e_id)
+	fmt.Println("Employee Name ", p1.name)
+	fmt.Println("Employee Department: ", p1.departments)
+	fmt.Println("Employee Skills: ", p1.skills)
+	for _, v := range p1.skills {
+		fmt.Printf("Employee Skill : %v \n", v)
 	}
 
-	c1 := Clients{
-		c_id:     1,
-		name:     "ABC Corporation",
-		industry: "Finance",
-		contact: []Contact{{
-			c_name: "Alice Johnson",
-			c_role: "CTO",
-		}, {
-			c_name: "Bob Williams",
-			c_role: "CFO",
-		},
-		},
-	}
+	// fmt.Println(p1.projects)
 
-	c2 := Clients{
-		c_id:     2,
-		name:     "XYZ Enterprises",
-		industry: "Retail",
-		contact: []Contact{{
-			c_name: "Emily Davis",
-			c_role: "Marketing Director",
-		}, {
-			c_name: "Jack Brown",
-			c_role: "CEO",
-		}},
-	}
+	// for _, r:= range Team.t_id {
+	// 	fmt.Println()
+	// }
 
-	fmt.Println(p1, p2, c1, c2)
+	// p2 := Employees{
+	// 	e_id:        2,
+	// 	name:        "Jane Smith",
+	// 	departments: "Marketing",
+	// 	skills:      []string{"Digital Marketing", "Content Creation", "SEO"},
+	// 	projects: []Projects{{
+	// 		name_p:   "Campaign A",
+	// 		duration: 4,
+	// 		team: []Team{{
+	// 			t_id: 5,
+	// 			role: "Marketing Manager"}, {
+	// 			t_id: 6,
+	// 			role: "Content Writer",
+	// 		},
+	// 		},
+	// 	},
+	// 	},
+	// }
+
+	// c1 := Clients{
+	// 	c_id:     1,
+	// 	name:     "ABC Corporation",
+	// 	industry: "Finance",
+	// 	contact: []Contact{{
+	// 		c_name: "Alice Johnson",
+	// 		c_role: "CTO",
+	// 	}, {
+	// 		c_name: "Bob Williams",
+	// 		c_role: "CFO",
+	// 	},
+	// 	},
+	// }
+
+	// c2 := Clients{
+	// 	c_id:     2,
+	// 	name:     "XYZ Enterprises",
+	// 	industry: "Retail",
+	// 	contact: []Contact{{
+	// 		c_name: "Emily Davis",
+	// 		c_role: "Marketing Director",
+	// 	}, {
+	// 		c_name: "Jack Brown",
+	// 		c_role: "CEO",
+	// 	}},
+	// }
+	// fmt.Println("--------------")
+	// fmt.Println(p1, p2, c1, c2)
+	// fmt.Println("--------------")
 
 }
