c86c6d97860c5b639bec2dad1586c2e888b9bc8a
abuzar <mabuzar533@gmail.com>
2024-05-27 19:33:50 +0500

    FuncGo

---
+++ GoFunc/VariadicParameter.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func Sum(ii ...int) {
+	fmt.Println(ii)
+	fmt.Printf("%T\n", ii)
+}
---
+++ GoFunc/i2Func.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Func with no parameter no return
+func Foo() {
+	fmt.Println("I'm Abuzar")
+}
+
+// func with 1 para but no returns
+func Bar(a string) {
+	fmt.Println("My name is ", a)
+
+}
+
+// func with 1 para 1 retunr
+func Aloha(s string) string {
+	return fmt.Sprint("They call me Mr.", s)
+}
+
+// func with 2 para 2 returns
+
+func Dogyears(name string, age int) (string, int) {
+	age *= 7
+	return fmt.Sprint(name, " is old in dog years"), age
+}
+
+// func (receiver) identifier(parameteres)(returns ){code}
