562f5a9dae85007dfecc2ecce67d7b79a0e4d2f7
abuzar <mabuzar533@gmail.com>
2024-05-29 15:42:42 +0500

    annyfunc

--- GoFunc/WriterInter.go
+++ GoFunc/WriterInter.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-	"log"
-	"os"
+	"bytes"
+	"fmt"
 )
 
 func WriteInter() {
-	f, err := os.Create("output.txt")
-	if err != nil {
-		log.Fatalf("error %s", err)
-	}
-	defer f.Close()
+	// f, err := os.Create("output.txt")
+	// if err != nil {
+	// 	log.Fatalf("error %s", err)
+	// }
+	// defer f.Close()
 
-	s := []byte("Hello Gophers")
+	// s := []byte("Hello Gophers")
 
-	_, err = f.Write(s)
+	// _, err = f.Write(s)
 
-	if err != nil {
-		log.Fatalf("error %s", err)
-	}
+	// if err != nil {
+	// 	log.Fatalf("error %s", err)
+	// }
+
+	b := bytes.NewBufferString("Hello ")
+	fmt.Println(b.String())
+	b.WriteString("Gophers")
+	fmt.Println(b.String())
+	b.Reset()
+	b.WriteString("Its thursday")
+
+	b.Write([]byte("Happ Happy"))
+	fmt.Println(b.String())
 }
