657ec186de2c1cd6a0f40b11e683d85640516679
abuzar <mabuzar533@gmail.com>
2024-06-05 13:14:55 +0500

    Type Interface

--- GoGenerics/TypeConstraint.go
+++ GoGenerics/TypeConstraint.go
@@ -15,9 +15,18 @@ func addT[T int | string | float64](a, b T) T {
 
 }
 
+type myNumbers interface {
+	int | float64 | string
+}
+
+func addMyNumbers[T myNumbers](a, b T) T {
+	return a + b
+}
+
 func TConst() {
 	fmt.Println(addI(1, 2))
 	fmt.Println(addF(1.2, 2))
 	fmt.Println(addT("3333", "333"))
+	fmt.Println(addMyNumbers("H", "ds"))
 
 }
---
+++ GoGenerics/go.mod
@@ -0,0 +1,3 @@
+module Example/Generics
+
+go 1.22.2
