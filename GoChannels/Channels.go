615f843df21c1e6974f402c6db4ff54d3cf7aafe
abuzar <mabuzar533@gmail.com>
2024-06-11 19:24:40 +0500

    Push Channels

--- GoChannels/Channels.go
+++ GoChannels/Channels.go
@@ -15,3 +15,37 @@ func Cha() {
 	fmt.Print("\n", <-c)
 
 }
+
+// Directional Channels
+
+func DirectionalChannels() {
+	c := make(chan int)
+	cr := make(<-chan int) //Receive Only Channel
+	cs := make(chan<- int) // Send Only Channel
+
+	fmt.Println("---------Start---------")
+	fmt.Printf("c\t%T\n", c)
+	fmt.Printf("cr\t%T\n", cr)
+	fmt.Printf("cs\t%T\n", cs)
+	fmt.Println("---------End---------")
+
+}
+
+// Send Only Channel
+
+func SendOnlyChannel() {
+	// Send Only Channel can
+	c := make(chan<- int, 2)
+	c <- 22
+	// move C before the Channel symbol syntax to fix and Execute
+	fmt.Println(c)
+
+}
+
+func ReceiveOnlyChannel() {
+	// Receive Only Channel can
+	c := make(<-chan int, 2)
+
+	fmt.Println(<-c)
+
+}
