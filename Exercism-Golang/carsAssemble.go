b64302aada55cc2c47186ce8bee6c9bebb2e547d
abuzar <mabuzar533@gmail.com>
2024-06-20 19:18:29 +0500

    CarsAssemble

---
+++ Exercism-Golang/carsAssemble.go
@@ -0,0 +1,24 @@
+package main
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	rateSuccess := successRate / 100
+	return rateSuccess * float64(productionRate)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	rateSuccess := successRate / 100
+	perhour := rateSuccess * float64(productionRate)
+	return int(perhour) / 60
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	groupOfTen := carsCount / 10
+	remainders := carsCount % 10
+	cost := (groupOfTen*95000 + remainders*10000)
+	return uint(cost)
+}
